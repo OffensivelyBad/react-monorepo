b254fe789eea4762bdbd589cb3450bd3af3ab73e
Shawn Roller <sroller@techstyle.com>
2021-04-28 10:28:55 -0400

    adds zustand config store

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,18 +1,25 @@
-import { APP_TITLE } from "@flipcards/common";
+import { APP_TITLE, useConfig } from "@flipcards/common";
 import * as React from "react";
 
 export function App(): React.ReactElement {
   const [count, setCount] = React.useState(0);
+  const configStore = useConfig();
+  const { environment, setEnvironment } = configStore;
+  const nextEnvironment = environment === "Development" ? "Production" : "Development";
 
   return (
     <div>
       <h1>Welcome to {APP_TITLE}!</h1>
       <p>
-        This is the main page of our application where you can confirm that it
-        is dynamic by clicking the button below.
+        The button below shows that this page is dynamic!
       </p>
       <p>Current count: {count}</p>
       <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      <p>
+        This shows how we can use a zustand store across packages
+      </p>
+      <p>Current environment: {environment}</p>
+      <button onClick={() => setEnvironment(nextEnvironment)}>Change Environment</button>
     </div>
   )
 }
--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,1 +1,18 @@
+import create from "zustand";
+
+type ConfigStore = {
+  environment: string,
+  setEnvironment: (newEnvironment: string) => void;
+}
+
+const useConfigStore = create<ConfigStore>(set => ({
+  environment: "Development",
+  setEnvironment: (newEnvironment: string) => set({ environment: newEnvironment })
+}));
+
+export const useConfig = (): ConfigStore => {
+  const store = useConfigStore();
+  return store;
+};
+
 export const APP_TITLE = "react-monorepo";
